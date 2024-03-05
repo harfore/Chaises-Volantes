ce0b5dca7760968f9e1a894c54e7d0b253602769
Chloé T. <chloe.thomas14@gmail.com>
2024-03-05 15:59:20 +0100

    connexion bdd to API

--- Back/app.js
+++ Back/app.js
@@ -1,8 +1,25 @@
-const express = require('express');
+const express = require("express");
+const { Pool } = require("pg");
+
 const app = express();
 
-app.use((req, res) => {
-    res.json({message:'Votre requête a bien été reçue !'});
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "chaisesVolantes",
+  password: "T0d&Capsul3!",
+  port: 5432,
+});
+
+app.get("/person", async (req, response) => {
+    try {
+        const queryResult = await pool.query('SELECT * FROM person');
+        console.log('Résultat de la requête :', queryResult.rows);
+        response.json(queryResult.rows);
+    } catch (err) {
+        console.error ("Erreur dexécution de la requête :", err);
+        response.status(500).json({error: "Erreur"})
+    }
 });
 
 module.exports = app;
