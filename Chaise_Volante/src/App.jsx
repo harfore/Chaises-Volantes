aa7228a2a16c549f55303ea28ce286c1b46fa28f
Hanaë <vernonhanae@gmail.com>
2024-03-06 16:19:16 +0100

    footer

--- Chaise_Volante/src/App.jsx
+++ Chaise_Volante/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from './Componenets/Header/Header'
-import Hero from './Componenets/Hero/Hero'
 import Footer from './Componenets/Footer/Footer';
 
 
@@ -9,7 +8,6 @@ function App() {
   return (
    <div>
     <Header />
-    <Hero />
     <Footer />
    </div>
   )
