eb85767174df7e39d8826be2edc52c8c04d1b1c3
Hanaë <vernonhanae@gmail.com>
2024-03-07 14:38:21 +0100

    Filtre

--- Chaise_Volante/src/App.jsx
+++ Chaise_Volante/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Componenets/Header/Header'
 import Footer from './Componenets/Footer/Footer';
+import Filtre from './Componenets/Filtre/Filtre';
 
 function App() {
 
   return (
    <div>
     <Header />
+    <Filtre />
     <Footer />
    </div>
   )
