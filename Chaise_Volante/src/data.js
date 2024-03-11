4758140ec75bbee8ba904569b6f03ca29d3eac6c
harfore <remiharfoush@gmail.com>
2024-03-11 16:40:19 +0100

    piece

--- Chaise_Volante/src/data.js
+++ Chaise_Volante/src/data.js
@@ -1,109 +1,391 @@
 
 //Pour rendre responsive le carrousel
 export const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 6,
-    },
-    desktop: {
-      breakpoint: { max: 2000, min: 800 },
-      items: 4,
-    },
-    tablet: {
-      breakpoint: { max: 800, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
-  
-  //Ici vous avez les produits qui vont s'afficher dans le carrousel. A FAIRE: rajouter les images que vous voulez; ajouter les ligne pour pouvoir afficher le prix. Cette const affiche seulement les produits de la categorie NEAUVEAUTé.
-  //Il faudra probablement répliquer la même const mais pour les propduits SALON, CUISINE, CHAMBRE, SALLE DE BAIN. Et ensuit exporter les const dans les respectifs fichiers
-  // Ps: les images des produits en vent sont dans le folder "photos" (src-assets-photos). A FAIRE: trouver des images produits pour la salle bain 
-  
-  const getAllData = async () => {
-    const response = await fetch('http://localhost:3000/allmeubles')
-    const data = await response.json()
-    console.log(data)
-    return data
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 6,
+  },
+  desktop: {
+    breakpoint: { max: 2000, min: 800 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 800, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
   }
+};
 
-  export const productData = await getAllData()
+//Ici vous avez les produits qui vont s'afficher dans le carrousel. A FAIRE: rajouter les images que vous voulez; ajouter les ligne pour pouvoir afficher le prix. Cette const affiche seulement les produits de la categorie NEAUVEAUTé.
+//Il faudra probablement répliquer la même const mais pour les propduits SALON, CUISINE, CHAMBRE, SALLE DE BAIN. Et ensuit exporter les const dans les respectifs fichiers
+// Ps: les images des produits en vent sont dans le folder "photos" (src-assets-photos). A FAIRE: trouver des images produits pour la salle bain 
 
-  // export const productData = [
-  //   {
-  //     id:1,
-  //     Photo: [{
-  //       "photo1":"../img/CanapeGris1.avif"
-  //     },
-  //     {
-  //       "photo2":"../img/img1.jpg"
-  //     }
-  //   ],
-  //     MeubleNom: "Grande table",
-  //     Prix:"test"
-  //   },
-  //   {
-  //     id:2,
-  //     Photo: [{
-  //       "photo1":"../img/img1.jpg"
-  //     },
-  //     {
-  //       "photo2":"../img/img1.jpg"
-  //     }
-  //   ],
-  //     MeubleNom: "Chaise",
-  //     Prix:"test"
-  //   },
-  //   {
-  //     id:3,
-  //     Photo: [{
-  //       "photo1":"../img/img3.jpg"
-  //     },
-  //     {
-  //       "photo2":"../img/img1.jpg"
-  //     }
-  //   ],
-  //     MeubleNom: "Table de chevet",
-  //     Prix:"test"
-  //   },
-  //   {
-  //     id:4,
-  //     Photo: [{
-  //       "photo1":"../img/CanapeGris1.avif"
-  //     },
-  //     {
-  //       "photo2":"../img/img1.jpg"
-  //     }
-  //   ],
-  //     MeubleNom: "Petite table",
-  //     Prix:"test"
-  //   },
-  //   {
-  //     id:5,
-  //     Photo: [{
-  //       "photo1":"../img/CanapeGris1.avif"
-  //     },
-  //     {
-  //       "photo2":"../img/img1.jpg"
-  //     }
-  //   ],
-  //     MeubleNom: "Canapé",
-  //     Prix:"test"
-  //   },
-  //   {
-  //     id:6,
-  //     Photo: [{
-  //       "photo1":"../img/CanapeGris1.avif"
-  //     },
-  //     {
-  //       "photo2":"../img/img1.jpg"
-  //     }
-  //   ],
-  //     MeubleNom: "Bibliothéque",
-  //     Prix:"test"
-  //   }
-  // ];
+const getAllData = async () => {
+  const response = await fetch('http://localhost:3000/allmeubles')
+  const data = await response.json()
+  console.log("data:" + data)
+  return data
+}
+
+// export const productData = await getAllData()
+
+export const productData = [
+  {
+    "MeubleNom": "Table à manger",
+    "Photo": [
+      {
+        "photo1": "../img/tableManger1.avif"
+      },
+      {
+        "photo2": "../img/tableManger2.avif"
+      },
+      {
+        "photo3": "../img/tableManger3.avif"
+      },
+      {
+        "photo4": "../img/tableManger4.avif"
+      }
+    ],
+    "Type": "Table",
+    "Prix": 409,
+    "PieceNom": "Cuisine"
+  },
+  {
+    "MeubleNom": "Table à manger extensible",
+    "Photo": [
+      {
+        "photo1": "../img/tableExtensible1.avif"
+      },
+      {
+        "photo2": "../img/tableExtensible2.avif"
+      },
+      {
+        "photo3": "../img/tableExtensible3.avif"
+      },
+      {
+        "photo4": "../img/tableExtensible4.avif"
+      }
+    ],
+    "Type": "Table",
+    "Prix": 379,
+    "PieceNom": "Cuisine"
+  },
+  {
+    "MeubleNom": "Table à manger vintage",
+    "Photo": [
+      {
+        "photo1": "../img/TableVintage1.avif"
+      },
+      {
+        "photo2": "../img/TableVintage2.avif"
+      },
+      {
+        "photo3": "../img/TableVintage3.avif"
+      },
+      {
+        "photo4": "../img/TableVintage4.avif"
+      }
+    ],
+    "Type": "Table",
+    "Prix": 599,
+    "PieceNom": "Cuisine"
+  },
+  {
+    "MeubleNom": "Table à manger à roulettes",
+    "Photo": [
+      {
+        "photo1": "../img/TableRoulettes1.avif"
+      },
+      {
+        "photo2": "../img/TableRoulettes2.avif"
+      },
+      {
+        "photo3": "../img/TableRoulettes3.avif"
+      },
+      {
+        "photo4": "../img/TableRoulettes4.avif"
+      }
+    ],
+    "Type": "Table",
+    "Prix": 359,
+    "PieceNom": "Cuisine"
+  },
+  {
+    "MeubleNom": "Chaise en velours noire",
+    "Photo": [
+      {
+        "photo1": "../img/ChaiseEnVeloursNoire1.avif"
+      },
+      {
+        "photo2": "../img/ChaiseEnVeloursNoire2.avif"
+      },
+      {
+        "photo3": "../img/ChaiseEnVeloursNoire3.avif"
+      },
+      {
+        "photo4": "../img/ChaiseEnVeloursNoire4.avif"
+      }
+    ],
+    "Type": "Chaise",
+    "Prix": 219,
+    "PieceNom": "Cuisine"
+  },
+  {
+    "MeubleNom": "Lit vintage",
+    "Photo": [
+      {
+        "photo1": "../img/litVintage1.avif"
+      },
+      {
+        "photo2": "../img/litVintage2.avif"
+      },
+      {
+        "photo3": "../img/litVintage3.avif"
+      },
+      {
+        "photo4": "../img/litVintage4.avif"
+      }
+    ],
+    "Type": "Lit",
+    "Prix": 299,
+    "PieceNom": "Chambre"
+  },
+  {
+    "MeubleNom": "Lit exotique",
+    "Photo": [
+      {
+        "photo1": "../img/LitExotique1.avif"
+      },
+      {
+        "photo2": "../img/LitExotique2.avif"
+      },
+      {
+        "photo3": "../img/LitExotique3.avif"
+      },
+      {
+        "photo4": "../img/LitExotique4.avif"
+      }
+    ],
+    "Type": "Lit",
+    "Prix": 599,
+    "PieceNom": "Chambre"
+  },
+  {
+    "MeubleNom": "Lit capitonné",
+    "Photo": [
+      {
+        "photo1": "../img/LitCapitonne1.avif"
+      },
+      {
+        "photo2": "../img/LitCapitonne2.avif"
+      },
+      {
+        "photo3": "../img/LitCapitonne3.avif"
+      },
+      {
+        "photo4": "../img/LitCapitonne4.avif"
+      }
+    ],
+    "Type": "Lit",
+    "Prix": 429,
+    "PieceNom": "Chambre"
+  },
+  {
+    "MeubleNom": "Étagère design",
+    "Photo": [
+      {
+        "photo1": "../img/etagereDesign1.avif"
+      },
+      {
+        "photo2": "../img/etagereDesign2.avif"
+      },
+      {
+        "photo3": "../img/etagereDesign3.avif"
+      },
+      {
+        "photo4": "../img/etagereDesign4.avif"
+      }
+    ],
+    "Type": "Étagère",
+    "Prix": 819,
+    "PieceNom": "Salon"
+  },
+  {
+    "MeubleNom": "Étagère à chaussures",
+    "Photo": [
+      {
+        "photo1": "../img/etagereChaussures1.avif"
+      },
+      {
+        "photo2": "../img/etagereChaussures2.avif"
+      },
+      {
+        "photo3": "../img/etagereChaussures3.avif"
+      },
+      {
+        "photo4": "../img/etagereChaussures4.avif"
+      }
+    ],
+    "Type": "Étagère",
+    "Prix": 29,
+    "PieceNom": "Salon"
+  },
+  {
+    "MeubleNom": "Étagère à 6 niveaux",
+    "Photo": [
+      {
+        "photo1": "../img/EtagereNiveau1.avif"
+      },
+      {
+        "photo2": "../img/EtagereNiveau2.avif"
+      },
+      {
+        "photo3": "../img/EtagereNiveau3.avif"
+      },
+      {
+        "photo4": "Back/photos/EtagereNiveau4.avif"
+      }
+    ],
+    "Type": "Étagère",
+    "Prix": 89,
+    "PieceNom": "Salon"
+  },
+  {
+    "MeubleNom": "Chaise en velours orange",
+    "Photo": [
+      {
+        "photo1": "../img/ChaiseEnVelours1.avif"
+      },
+      {
+        "photo2": "../img/ChaiseEnVelours2.avi"
+      },
+      {
+        "photo3": "../img/ChaiseEnVelours3.avi"
+      },
+      {
+        "photo4": "../img/ChaiseEnVelours4.avi"
+      }
+    ],
+    "Type": "Chaise",
+    "Prix": 209,
+    "PieceNom": "Cuisine"
+  },
+  {
+    "MeubleNom": "Canapé convertible style scandinave",
+    "Photo": [
+      {
+        "photo1": "../img/CanapeScandinave1.avif"
+      },
+      {
+        "photo2": "../img/CanapeScandinave2.avif"
+      },
+      {
+        "photo3": "../img/CanapeScandinave3.avif"
+      },
+      {
+        "photo4": "../img/CanapeScandinave4.avif"
+      }
+    ],
+    "Type": "Canapé",
+    "Prix": 799,
+    "PieceNom": "Salon"
+  },
+  {
+    "MeubleNom": "Canapé clic-clac en velours",
+    "Photo": [
+      {
+        "photo1": "../img/ClicClac1.avif"
+      },
+      {
+        "photo2": "../img/ClicClac1.avif"
+      },
+      {
+        "photo3": "../img/ClicClac1.avif"
+      },
+      {
+        "photo4": "../img/ClicClac1.avif"
+      }
+    ],
+    "Type": "Canapé",
+    "Prix": 479,
+    "PieceNom": "Salon"
+  },
+  {
+    "MeubleNom": "Canapé en velours côtelé beige",
+    "Photo": [
+      {
+        "photo1": "../img/canapeBeige1.avif"
+      },
+      {
+        "photo2": "../img/canapeBeige2.avif"
+      },
+      {
+        "photo3": "../img/canapeBeige3.avif"
+      },
+      {
+        "photo4": "../img/canapeBeige4.avif"
+      }
+    ],
+    "Type": "Canapé",
+    "Prix": 999,
+    "PieceNom": "Salon"
+  },
+  {
+    "MeubleNom": "Canapé en velours côtelé gris clair",
+    "Photo": [
+      {
+        "photo1": "../img/CanapeGris1.avif"
+      },
+      {
+        "photo2": "../img/CanapeGris2.avif"
+      },
+      {
+        "photo3": "../img/CanapeGris3.avif"
+      },
+      {
+        "photo4": "../img/CanapeGris4.avif"
+      }
+    ],
+    "Type": "Canapé",
+    "Prix": 1299,
+    "PieceNom": "Salon"
+  },
+  {
+    "MeubleNom": "Canapé beige sable",
+    "Photo": [
+      {
+        "photo1": "../img/CanapeSable1.avif"
+      },
+      {
+        "photo2": "../img/CanapeSable2.avif"
+      },
+      {
+        "photo3": "../img/CanapeSable3.avif"
+      },
+      {
+        "photo4": "../img/CanapeSable4.avif"
+      }
+    ],
+    "Type": "Canapé",
+    "Prix": 1199,
+    "PieceNom": "Salon"
+  }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
